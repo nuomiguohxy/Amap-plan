c48cb8f4bd1a8ed559a9b7cfece76c626422626b
xinyi.huang <251785120@qq.com>
2018-01-13 20:02:50 +0800

    the initial plan
    initial this app

---
+++ js/filter.js
@@ -0,0 +1,34 @@
+/**
+ * Created by user on 2018/1/7.
+ */
+//function viewmodel(locations){
+//    console.log(locations);
+//    var self = this;
+//    self.address = ko.observableArray(["1","2","hello"]);
+//}
+// var LatLng = function(latlgn){
+//      this.M = ko.observable(latlgn.M);
+//      this.O = ko.observable(latlgn.O);
+//      this.lat = ko.observable(latlgn.lat);
+//      this.lng = ko.observable(latlgn.lng);
+//  };
+//var position =function(data){
+//    this.name = ko.observable(data.name);
+//    this.location = ko.observable(new LatLng(data.location));
+//    this.address = ko.observable(data.address);
+//};
+//var viewModel = function(){
+//    var self = this;
+//    this.locationList = ko.observableArray([]);
+//    locations.forEach(function(locationItem){
+//         self.locationList.push(new position(locationItem));
+//    });
+//};
+//ko.applyBindings(new viewModel());
+////
+////function startBinding(locations){
+////
+////    console.log("调用成功");
+////    ko.applyBindings(new viewModel(locations));
+////}
+
